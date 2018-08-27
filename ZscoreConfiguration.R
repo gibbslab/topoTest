54987d0f7f072286f7e835e37d2e7daf379220d3
juancholkovich <judhenaosa@unal.edu.co>
2018-08-27 18:49:06 -0500

    first function

--- ZscoreConfiguration.R
+++ ZscoreConfiguration.R
@@ -3,21 +3,13 @@ library(igraph)
 input <- read.graph("~/Documentos/Data3.0/AZ/AlzheimerN.net",format = "ncol")
 loops <- FALSE
 
-ddist <- table(sort(as.vector(degree(input,mode = "total")),decreasing = FALSE))
+ddist <- as.data.frame.numeric(table(sort(as.vector(degree(input,mode = "total")),decreasing = FALSE)))
 
 empty <- make_empty_graph(n = vcount(input),directed = FALSE)
 
 nodes <- seq(vcount(input))
 
-for(a in ddist){
-  val <- sample(nodes,1)
-  nodes <- nodes[!nodes %in% val]
+sapply(ddist,function(x){
   
-  edge <- sample(nodes,a)
-  
-  for(b in edge){
-    edge_list <- c(edge_list,val,b)
-  }
-}
+})
 
-empty <- empty %>% add_edges(edge_list)

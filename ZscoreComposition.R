abc558958e707cde4d96684792860ed7e918c8fa
juancholkovich <judhenaosa@unal.edu.co>
2018-08-27 15:15:24 -0500

    first function

--- ZscoreComposition.R
+++ ZscoreComposition.R
@@ -1,8 +1,23 @@
 library(igraph)
 
 input <- read.graph("~/Documentos/Data3.0/AZ/AlzheimerN.net",format = "ncol")
+loops <- TRUE
+
 ddist <- sort(as.vector(degree(input,mode = "total")),decreasing = TRUE)
 
 empty <- make_empty_graph(n = vcount(input),directed = FALSE)
 
 nodes <- seq(vcount(input))
+val <- sample(nodes,1)
+
+for(a in seq(length(ddist))){
+  for(i in seq(ddist[a])){
+    edge <- sample(val,1)
+    if(loops  == TRUE){
+      while(edge == val){
+        edge <- sample(val,1)
+      }
+    }
+    empty <- empty + edge(val,edge)
+  }
+}

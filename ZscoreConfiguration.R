ee4537710720105f529f31e64b4107b6bec46631
juancholkovich <judhenaosa@unal.edu.co>
2018-08-27 22:19:12 -0500

    first function

--- ZscoreConfiguration.R
+++ ZscoreConfiguration.R
@@ -9,24 +9,30 @@ empty <- make_empty_graph(n = vcount(input),directed = FALSE)
 
 nodes <- seq(vcount(input))
 
-for(i in ddist){
-  alfa <- as.numeric(names(i)) - max(degree(empty))
-
+for(i in seq(dim(ddist)[1])){
+  alfa <- as.numeric(names(ddist[i,])) - max(degree(empty))
   for(j in seq(alfa)){
-
     if(length(nodes)%%2==0){
       resample <- sample(nodes,length(nodes))
-
     }else{
       resample <- sample(nodes,length(nodes)-1)
     }
     empty <- empty %>% add_edges(resample)
   }
   
-  del <- sample(nodes,i)
-  print(del)
+  del <- sample(nodes,ddist[i,])
   nodes <- nodes[!nodes %in% del]
 }
 
 
 
+last <- which(as.vector(sort(degree(input))) != sort(degree(empty)))
+
+empty <- empty %>% add_edges(last)
+
+
+
+
+
+as.vector(sort(degree(input))) != sort(degree(empty))
+sort(degree(empty))

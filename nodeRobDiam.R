6323e14a60553d283c62708f6b33890a7f482fb3
juancholkovich <judhenaosa@unal.edu.co>
2018-08-29 13:14:24 -0500

    robustness diameter

--- nodeRobDiam.R
+++ nodeRobDiam.R
@@ -1,15 +1,19 @@
 library(igraph)
 
 input <- read.graph("~/Documentos/Coquito/MM_coquitoCyto.txt","ncol")
+input <- read.graph("~/Documentos/Data3.0/AZ/AlzheimerN.net","ncol")
 
 fraction <- 0.05
 
 hubs <- names(sort(degree(input),decreasing = TRUE))
 input.hubs <- input
+before_array <- ""
 
 for(i in seq(0.005,fraction,0.005)){
   to_delete <- hubs[1:round(vcount(input)*i)]
-  input.hubs <- input.hubs - vertices(to_delete)
+  print(length(to_delete))
+  input.hubs <- input.hubs - vertices(to_delete[which(to_delete!=before_array)])
+  before_array <- to_delete
 }
 
 clustering <- data.frame(transitivity(input,type = "local",isolates = "zero"),names(V(input)))

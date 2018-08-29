6258ba82e518359ab1c61c8648c5f2160ec03609
juancholkovich <judhenaosa@unal.edu.co>
2018-08-28 20:14:41 -0500

    robustness diameter

--- nodeRobDiam.R
+++ nodeRobDiam.R
@@ -4,3 +4,9 @@ input <- read.graph("~/Documentos/Coquito/MM_coquitoCyto.txt","ncol")
 
 hubs <- names(sort(degree(input),decreasing = TRUE))
 clustering <- data.frame(transitivity(input,type = "local",isolates = "zero"),names(V(input)))
+names(clustering) <- c("value","ID")
+clustering <- clustering[sort(clust)]
+betweenness <- names(sort(betweenness(input,directed = FALSE),decreasing = TRUE))
+random <- sample(hubs,length(hubs))
+
+clustering[sort(clustering$value,decreasing = TRUE),]

86537272bbf8869c7bcffe4e6e552b0240009b33
juancholkovich <judhenaosa@unal.edu.co>
2018-09-03 17:11:30 -0500

    node robustness

--- nodeRobDiam.R
+++ nodeRobDiam.R
@@ -6,22 +6,29 @@ input <- read.graph("~/Documentos/Data3.0/AZ/AlzheimerN.net","ncol")
 fraction <- 0.05
 
 hubs <- names(sort(degree(input),decreasing = TRUE))
-input.robus <- input
-before_array <- ""
+random <- sample(hubs,length(hubs))
+
+input.hubs <- input.random <- input
+before_array <- 0
 
 for(i in seq(0.005,fraction,0.005)){
-  to_delete <- hubs[1:round(vcount(input)*i)]
-  input.robus <- input.robus - vertices(as.vector(na.omit(to_delete[length(before_array)+1:length(to_delete)])))
+  to_delete <- round(vcount(input)*i)
+  print(to_delete)
+  input.hubs <- input.hubs - vertices(as.vector(na.omit(hubs[before_array+1:to_delete])))
+  
+  
+  diam <- diameter(input.robus)
+  
   before_array <- to_delete
 }
 
-
+as.vector(na.omit(hubs[before_array+1:to_delete]))
 
 clustering <- data.frame(transitivity(input,type = "local",isolates = "zero"),names(V(input)))
 names(clustering) <- c("value","ID")
 clustering <- clustering[with(clustering,order(clustering$value,decreasing = TRUE)),]$ID
 betweenness <- names(sort(betweenness(input,directed = FALSE),decreasing = TRUE))
-random <- sample(hubs,length(hubs))
+
 
 
 

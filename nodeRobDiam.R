589b4e671967791feddb5343d6dd7cca73ee3e1a
juancholkovich <judhenaosa@unal.edu.co>
2018-09-03 17:30:59 -0500

    node robustness

--- nodeRobDiam.R
+++ nodeRobDiam.R
@@ -14,8 +14,10 @@ before_array <- 0
 for(i in seq(0.005,fraction,0.005)){
   to_delete <- round(vcount(input)*i)
   print(to_delete)
-  input.hubs <- input.hubs - vertices(as.vector(na.omit(hubs[before_array+1:to_delete])))
-  
+  if(to_delete > before_array){
+    input.hubs <- input.hubs - vertices(as.vector(na.omit(hubs[(before_array+1):to_delete])))
+    input.random <- input.random - vertices(as.vector(na.omit(random[(before_array+1):to_delete])))
+  }
   
   diam <- diameter(input.robus)
   
@@ -29,10 +31,3 @@ names(clustering) <- c("value","ID")
 clustering <- clustering[with(clustering,order(clustering$value,decreasing = TRUE)),]$ID
 betweenness <- names(sort(betweenness(input,directed = FALSE),decreasing = TRUE))
 
-
-
-
-
-
-
-seq(0,fraction,0.005)

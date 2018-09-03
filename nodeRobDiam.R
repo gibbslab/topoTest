42a45242a6a354a58ea3cba088c38226520ce9ca
juancholkovich <judhenaosa@unal.edu.co>
2018-09-03 18:03:01 -0500

    node robustness

--- nodeRobDiam.R
+++ nodeRobDiam.R
@@ -4,6 +4,7 @@ input <- read.graph("~/Documentos/Coquito/MM_coquitoCyto.txt","ncol")
 input <- read.graph("~/Documentos/Data3.0/AZ/AlzheimerN.net","ncol")
 
 fraction <- 0.05
+centrality <- NULL
 
 hubs <- names(sort(degree(input),decreasing = TRUE))
 random <- sample(hubs,length(hubs))
@@ -11,20 +12,30 @@ random <- sample(hubs,length(hubs))
 input.hubs <- input.random <- input
 before_array <- 0
 
+diam.hub <- diam.ranom <- diameter(input)
+
+
+result <- data.frame(seq(0,fraction,0.005))
+names(result) <- "fr"
+
 for(i in seq(0.005,fraction,0.005)){
   to_delete <- round(vcount(input)*i)
-  print(to_delete)
+  
   if(to_delete > before_array){
     input.hubs <- input.hubs - vertices(as.vector(na.omit(hubs[(before_array+1):to_delete])))
     input.random <- input.random - vertices(as.vector(na.omit(random[(before_array+1):to_delete])))
   }
   
-  diam <- diameter(input.robus)
+  
+  
+  diam.hub <- c(diam.hub,diameter(input.hubs))
+  diam.ranom <- c(diam.ranom,diameter(input.random))
   
   before_array <- to_delete
 }
 
-as.vector(na.omit(hubs[before_array+1:to_delete]))
+result$f_hub <- diam.hub
+result$f_random <- diam.ranom
 
 clustering <- data.frame(transitivity(input,type = "local",isolates = "zero"),names(V(input)))
 names(clustering) <- c("value","ID")

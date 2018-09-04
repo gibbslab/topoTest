ca7ab32e807b5a337dffdaeb60bfaa67ed5120c1
juancholkovich <judhenaosa@unal.edu.co>
2018-09-03 19:20:27 -0500

    node robustness

--- nodeRobDiam.R
+++ nodeRobDiam.R
@@ -4,6 +4,7 @@ input <- read.graph("~/Documentos/Coquito/MM_coquitoCyto.txt","ncol")
 input <- read.graph("~/Documentos/Data3.0/AZ/AlzheimerN.net","ncol")
 
 fraction <- 0.05
+by <- 0.005
 centrality <- NULL
 
 hubs <- names(sort(degree(input),decreasing = TRUE))
@@ -15,10 +16,28 @@ before_array <- 0
 diam.hub <- diam.ranom <- diameter(input)
 
 
-result <- data.frame(seq(0,fraction,0.005))
+result <- data.frame(seq(0,fraction,by))
 names(result) <- "fr"
 
-for(i in seq(0.005,fraction,0.005)){
+if(centrality == "clustering"){
+  diam.centrality <- diameter(input)
+  input.centrality <- input
+  clustering <- data.frame(transitivity(input,type = "local",isolates = "zero"),names(V(input)))
+  names(clustering) <- c("value","ID")
+  clustering <- clustering[with(clustering,order(clustering$value,decreasing = TRUE)),]$ID
+}else if(centrality == "betwenness"){
+  diam.centrality <- diameter(input)
+  input.centrality <- input
+  betweenness <- names(sort(betweenness(input,directed = FALSE),decreasing = TRUE))
+}else if(file.exists(centrality)){
+  diam.centrality <- diameter(input)
+  input.centrality <- input
+  file.centrality <- read.table(centrality)
+}else if(!is.null(centrality)){
+  stop("The selected option is invalid")
+}
+
+for(i in seq(by,fraction,by)){
   to_delete <- round(vcount(input)*i)
   
   if(to_delete > before_array){
@@ -37,8 +56,4 @@ for(i in seq(0.005,fraction,0.005)){
 result$f_hub <- diam.hub
 result$f_random <- diam.ranom
 
-clustering <- data.frame(transitivity(input,type = "local",isolates = "zero"),names(V(input)))
-names(clustering) <- c("value","ID")
-clustering <- clustering[with(clustering,order(clustering$value,decreasing = TRUE)),]$ID
-betweenness <- names(sort(betweenness(input,directed = FALSE),decreasing = TRUE))
 
